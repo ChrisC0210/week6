702cbad862c08e5e8a1576fa2942afd0c3442712
chris chen <christinachen149@gmail.com>
2022-09-15 00:05:27 +0800

    Update 2022-09-14T16:04:33.989Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,10 +1,22 @@
 "use strict";
 
 $(function () {
-  $('.grid').masonry({
-    // options...
-    itemSelector: '.grid-item',
-    columnWidth: 200
+  // Swiper
+  var swiper = new Swiper('.swiper', {
+    // Optional parameters
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination'
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    }
   });
 });
 //# sourceMappingURL=all.js.map
